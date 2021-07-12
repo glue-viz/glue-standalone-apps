0e120fe0d727268b7f55b44ec8050eefd8e2a4a8
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-07-12 16:04:40 +0100

    Fix hook

--- hooks/hook-glue_vispy_viewers.py
+++ hooks/hook-glue_vispy_viewers.py
@@ -1,5 +1,4 @@
-
 from PyInstaller.utils.hooks import collect_data_files, copy_metadata
 
-datas = collect_data_files('glue-vispy-viewers')
+datas = collect_data_files('glue_vispy_viewers')
 datas += copy_metadata('glue-vispy-viewers')
