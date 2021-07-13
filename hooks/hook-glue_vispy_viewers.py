ac2b2d367a63c0fd5ed8aa8f8406834be5a8ee7e
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-07-13 11:06:27 +0100

    Fix WWT viewer and other issues

--- hooks/hook-glue_vispy_viewers.py
+++ hooks/hook-glue_vispy_viewers.py
@@ -1,4 +1,4 @@
 from PyInstaller.utils.hooks import collect_data_files, copy_metadata
 
-datas = collect_data_files('glue_wwt', include_py_files=True)
-datas += copy_metadata('glue-wwt')
+datas = collect_data_files('glue_vispy_viewers', include_py_files=True)
+datas += copy_metadata('glue-vispy-viewers')
--- start_glue.py
+++ start_glue.py
@@ -13,8 +13,21 @@ if __name__ == "__main__":
 
     ga = GlueApplication()
 
-    if '--nonblocking' in sys.argv:
+    if '--test' in sys.argv:
+
         ga.start(block=False)
+
+        # Open a few viewers to test
+
+        from glue.viewers.image.qt import ImageViewer
+        ga.new_data_viewer(ImageViewer)
+
+        from glue_wwt.viewer.qt_data_viewer import WWTQtViewer
+        ga.new_data_viewer(WWTQtViewer)
+
+        from glue_vispy_viewers.scatter.scatter_viewer import VispyScatterViewer
+        ga.new_data_viewer(VispyScatterViewer)
+
         start = time.time()
         print("Waiting 5 seconds before closing...")
         while time.time() - start < 5:
