542595090073ffb5f7905f34a54596c021ceeea3
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-10-14 14:26:01 +0100

    Fix MacOS X output path

--- osx/make_dmg.sh
+++ osx/make_dmg.sh
@@ -5,5 +5,5 @@ rm -rf dist/start_glue
 python osx/fix_app_qt_folder_names_for_codesign.py dist/"glue $1.app"
 codesign -s - --force --all-architectures --timestamp --deep dist/"glue $1.app"
 dist/glue.app/Contents/MacOS/start_glue --test
-hdiutil create -volname "Glue" -srcfolder dist -ov -format UDZO "glue $1.dmg"
+hdiutil create -volname "Glue" -srcfolder dist -ov -format UDZO dist/"glue $1.dmg"
 rm -rf dist/"glue $1.app"
