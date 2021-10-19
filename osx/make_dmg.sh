f45d71fec9792972949d38bf70bf7a2d206d62ff
Thomas Robitaille <thomas.robitaille@gmail.com>
2021-10-19 15:32:26 +0100

    Patch make_dmg.sh

--- osx/make_dmg.sh
+++ osx/make_dmg.sh
@@ -4,6 +4,6 @@ mv dist/glue.app dist/"glue $1.app"
 rm -rf dist/start_glue
 python osx/fix_app_qt_folder_names_for_codesign.py dist/"glue $1.app"
 codesign -s - --force --all-architectures --timestamp --deep dist/"glue $1.app"
-dist/glue.app/Contents/MacOS/start_glue --test
+dist/glue $1.app/Contents/MacOS/start_glue --test
 hdiutil create -volname "Glue" -srcfolder dist -ov -format UDZO dist/"glue $1.dmg"
 rm -rf dist/"glue $1.app"
