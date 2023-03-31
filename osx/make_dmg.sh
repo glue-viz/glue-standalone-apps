1e37c865ed10d7473885a0441d748508debeebc5
Thomas Robitaille <thomas.robitaille@gmail.com>
2023-03-31 13:13:06 +0100

    Remove old test command

--- osx/make_dmg.sh
+++ osx/make_dmg.sh
@@ -5,6 +5,5 @@ rm -rf dist/start_glue
 python osx/fix_app_qt_folder_names_for_codesign.py dist/"glue $1.app"
 codesign --force --deep --sign - dist/"glue $1.app"
 dist/"glue $1.app"/Contents/MacOS/start_glue --test
-dist/glue $1.app/Contents/MacOS/start_glue --test
 hdiutil create -volname "Glue" -srcfolder dist -ov -format UDZO dist/"glue $1.dmg"
 rm -rf dist/"glue $1.app"
